58c88bb5953f77bb88bb0a32bb4226d048c1ae66
Hugo Trippaers <htrippaers@schubergphilis.com>
2013-02-17 13:50:57 +0100

    Change licensing to Apache License 2.0

--- calculator.c
+++ calculator.c
@@ -1,10 +1,21 @@
-/* $Id: calculator.c,v 1.2 2008/07/18 07:16:00 cvsd Exp $
+/**
+ * Copyright 2008 Hugo Trippaers <opensource@strocamp.net>
  *
- *  Implementation of A* algorithm
- *  used to find the shortes jump route between
- *  systems
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  Implementation of A* algorithm used to find the shortes jump route 
+ *  between systems.
  */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
